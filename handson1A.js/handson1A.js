16c70e1324bc729bfbec46b9516c7b19282987a1
Garcia_katrisse <angelcnmyxcc@gmail.com>
2025-10-06 13:49:55 +0800

    M.Handson

--- handson1A.js/handson1A.js
+++ handson1A.js/handson1A.js
@@ -8,14 +8,24 @@ function CheckGrade() {
     console.log(finalgrade);
     //finalgrade > = 76 
 
-     if (midtermgrade = null) {
-        console.log("please enter your midterm grade");
-     else if (finalgrade = null) {
-        console,log(please enter your final grade);
-        else { 
-            let average = ()
-        }
-     }
+     if (finalgrade >= 75 && midtermgrade === "") {
+     
+        console.log("Please input your midterm & final grade");
      }
 
-}
+     else if (midtermgrade === "") {
+         console.log("please input your midterm grade");
+
+     } else if (finalgrade === "") {
+          console.log("please input your final grade");
+
+     } else if (midtermgrade >= 75 && finalgrade >= 75) {
+         console.log("PASSED"); 
+     } else if ("midtermgrade <75 && finalgrade <75") {
+      console.log("FAILED");
+     }
+     }
+     
+     document.getElementById("Compute").onclick = function() {CheckGrade()};
+      
+     
